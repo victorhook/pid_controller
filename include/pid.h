12bb6cdac612522b64cb8fb52c9a4d1be96f83e9
Victor <victorkrook96@gmail.com>
2021-04-30 09:26:30 +0200

    Created a simple pid-controller. Base functionality working

--- include/pid.h
+++ include/pid.h
@@ -1,8 +1,33 @@
 #ifndef PID_H
 #define PID_H
 
+#include <limits>
+#define OUTPUT_MIN_VALUE std::numeric_limits<float>::min()
+#define OUTPUT_MAX_VALUE std::numeric_limits<float>::max()
+
+
 class PidController {
 
+    private:
+        float Kp;
+        float Ki;
+        float Kd;
+        float proportinal;
+        float integral;
+        float derivative;
+        float last_error;
+        float dt;
+        float setPoint;
+        float outputMin;
+        float outputMax;
+
+        float limit(const float output) const;
+
+    public:
+        PidController(float Kp, float Ki, float Kd, float setPoint,
+                      float outputMin, float outputMax);
+        ~PidController();
+        float update(float value);
 };
 
 
--- src/pid.cpp
+++ src/pid.cpp
@@ -1,1 +1,48 @@
 #include "pid.h"
+
+
+PidController::PidController(float Kp, float Ki, float Kd, float setPoint,
+                             float outputMin=OUTPUT_MIN_VALUE, 
+                             float outputMax=OUTPUT_MAX_VALUE) 
+    : Kp(Kp), Ki(Ki), Kd(Kd), setPoint(setPoint), outputMin(outputMin),
+      outputMax(outputMax)
+{
+    dt = 0.1;
+    last_error = 0;
+}
+
+PidController::~PidController() 
+{
+    
+}
+
+float PidController::update(float value) 
+{
+    float error = setPoint - value;
+
+    // P
+    proportinal = Kp * error;
+
+    // I
+    integral += Ki * integral * error;
+
+    // D
+    derivative = Kd * ( (error - last_error) / dt );
+
+    // Sum output
+    float output = proportinal + integral + derivative;
+
+    // Ensure the output value is within the limits.
+    output = limit(output);
+
+    return output;
+}
+
+float PidController::limit(const float output) const
+{
+    if (output <= outputMin)
+        return outputMin;
+    else if (output >= outputMax)
+        return outputMax;
+    return output;
+}
